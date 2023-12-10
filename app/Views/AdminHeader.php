e74f3f3913970566d9633b167e8917b97a74784b
Muhammad ridho <ridhom748@gmail.com>
2023-12-10 19:22:40 +0700

    z

--- app/Views/AdminHeader.php
+++ app/Views/AdminHeader.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Ridho | Dashboard</title>
+  <title>Salary | Dashboard</title>
   <base href="<?= base_url("assets") ?>/">
 
 
